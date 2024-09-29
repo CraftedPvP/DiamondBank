366d61b25f81bc28bb57d1c0bb9243ea420be702
Michael Ong <20311462+CraftedPvP@users.noreply.github.com>
2024-09-29 09:26:56 +0800

    Fixed form input with variants

--- src/UI/FormTest.cpp
+++ src/UI/FormTest.cpp
@@ -1,35 +1,38 @@
 #include "FormTest.h"
 #include <iostream>
-#include <iomanip> // std::setw
 
 using std::cout;
 using std::cin;
 using std::cerr;
 using std::endl;
-using std::setw;
 
 void FormTest::Initialize()
 {
     FormQuestion question;
-    // question.Set<std::string>("Are you a hotdog?","");
-    std::string questionn = "Are you a hotdog?";
-    question.Set<float>(questionn,0);
+    question.Set<std::string>("Are you a hotdog?","");
     formData.emplace("1",question);
+
+    question.Set<float>("what hotdog # are you?",0);
+    formData.emplace("2",question);
 }
 void FormTest::Content()
 {
     // cout << formData["1"].GetQuestion() << endl;
     int num = 1;
-    for(auto kv : formData){//kv.second.GetQuestion()
-        cout << num << ". " << kv.second.GetQuestion() << setw(10) << std::right;
+    for(auto& kv : formData){//kv.second.GetQuestion()
+        cout << num << ". " << kv.second.GetQuestion() << "\t";
         kv.second.GetInput();
+        num++;
     }
 }
 
 void FormTest::AskForInput()
 {
-    float res = formData["1"].GetResponse<float>();
+    std::string res = formData["1"].GetResponse<std::string>();
     // float res = std::get<float>(formData["1"].GetResponse2());
     cout << "the truth is - " << res << endl;
-    formData["1"].PrintResponse();
+
+    float res2 = formData["2"].GetResponse<float>();
+    // float res = std::get<float>(formData["1"].GetResponse2());
+    cout << "the hotdog # is - " << res2 << endl;
 }
